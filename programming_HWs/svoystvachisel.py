75e8f6276bf82bb086da1d227710418b2e9f6fcd
eischaire <anohap@mail.ru>
2016-10-16 21:15:53 +0300

    Add files via upload
    Домашние задания по программированию

---
+++ programming_HWs/svoystvachisel.py
@@ -0,0 +1,14 @@
+a = input('введите число а: ')
+b = input('введите число b: ')
+c = input('введите число c: ')
+a = int(a)
+b = int(b)
+c = int(c)
+if (a + b) == c :
+    print('a+b=c')
+else :
+    print('a+b=/=c')
+if (a*c)==(0-b) :
+    print('a*c+b=0')
+else :
+    print('a*c+b=/=0')
